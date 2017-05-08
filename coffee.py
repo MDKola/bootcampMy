14f9825dcf18561912c5dac5b3476da71a2afaac
kolaempire <denokolas@gmail.com>
2017-05-08 15:10:54 +0300

    added coffee.py and test.txt

---
+++ coffee.py
@@ -0,0 +1,21 @@
+coffee = "tasty coffee"
+def coffee_maker():
+    print ("take cup")
+    print ("pour coffee into cup")
+    print ("add sugar")
+    print("pour milk")
+    print ("stirr")
+    
+    return coffee
+
+
+def serve_coffee(coffee,name):
+     name = "kola"
+     return name
+name = "kola"
+print (coffee + " " + name)
+    
+    
+    
+    
+    
